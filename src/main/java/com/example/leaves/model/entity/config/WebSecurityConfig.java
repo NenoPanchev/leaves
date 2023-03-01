461a219e4f978089c826741f2a47aba19e44ab27
neno <neno.panchev@gmail.com>
2023-03-01 17:49:27 +0200

    rest security

---
+++ src/main/java/com/example/leaves/model/entity/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.leaves.model.entity.config;
+
+import com.example.leaves.service.impl.AppUserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableGlobalMethodSecurity(
+//         securedEnabled = true,
+//         jsr250Enabled = true,
+        prePostEnabled = true)
+
+public class WebSecurityConfig {
+    private final AppUserDetailService userDetailService;
+
+    public WebSecurityConfig(AppUserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests().antMatchers("/users/**").permitAll()
+                .anyRequest().authenticated();
+
+        http.authenticationProvider(authenticationProvider());
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
