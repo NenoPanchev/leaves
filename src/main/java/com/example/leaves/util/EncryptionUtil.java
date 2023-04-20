9bd4d12836c31208fc470da5e1e87cd2d25a6245
V.Kacharov <vladimir.kacharov@lightsoftbulgaria.com>
2023-04-20 10:36:18 +0300

    validation

--- src/main/java/com/example/leaves/util/EncryptionUtil.java
+++ src/main/java/com/example/leaves/util/EncryptionUtil.java
@@ -19,47 +19,53 @@ public class EncryptionUtil {
 
     /* Encryption Method */
     public static String encrypt(String strToEncrypt) {
-        try {
-            /* Declare a byte array. */
-            byte[] iv = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            /* Create factory for secret keys. */
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            /* PBEKeySpec class implements KeySpec interface. */
-            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
-            /* Retruns encrypted value. */
-            return Base64.getEncoder()
-                    .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
-        } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException |
-                 InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e) {
-            System.out.println("Error occured during encryption: " + e.toString());
+        if (strToEncrypt!=null) {
+            try {
+                /* Declare a byte array. */
+                byte[] iv = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
+                IvParameterSpec ivspec = new IvParameterSpec(iv);
+                /* Create factory for secret keys. */
+                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                /* PBEKeySpec class implements KeySpec interface. */
+                KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
+                SecretKey tmp = factory.generateSecret(spec);
+                SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+                /* Retruns encrypted value. */
+                return Base64.getEncoder()
+                        .encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
+            } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException |
+                     InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException |
+                     NoSuchPaddingException e) {
+                System.out.println("Error occured during encryption: " + e.toString());
+            }
         }
         return null;
     }
 
     /* Decryption Method */
     public static String decrypt(String strToDecrypt) {
-        try {
-            /* Declare a byte array. */
-            byte[] iv = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-            /* Create factory for secret keys. */
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            /* PBEKeySpec class implements KeySpec interface. */
-            KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-            /* Retruns decrypted value. */
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException |
-                 InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e) {
-            System.out.println("Error occured during decryption: " + e);
+        if (strToDecrypt!=null) {
+            try {
+                /* Declare a byte array. */
+                byte[] iv = {0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0};
+                IvParameterSpec ivspec = new IvParameterSpec(iv);
+                /* Create factory for secret keys. */
+                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                /* PBEKeySpec class implements KeySpec interface. */
+                KeySpec spec = new PBEKeySpec(SECRET_KEY.toCharArray(), SALTVALUE.getBytes(), 65536, 256);
+                SecretKey tmp = factory.generateSecret(spec);
+                SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+                cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+                /* Retruns decrypted value. */
+                return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+            } catch (InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException |
+                     InvalidKeySpecException | BadPaddingException | IllegalBlockSizeException |
+                     NoSuchPaddingException e) {
+                System.out.println("Error occured during decryption: " + e);
+            }
         }
         return null;
     }
