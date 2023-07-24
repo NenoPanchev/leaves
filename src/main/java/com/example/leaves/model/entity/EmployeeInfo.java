d6b9d63396bbde065426fb3f5ed4d8527895c1c9
Neno Panchev <neno.panchev@lightsoftbulgaria.com>
2023-07-24 16:32:40 +0300

    removed init stuff

--- src/main/java/com/example/leaves/model/entity/EmployeeInfo.java
+++ src/main/java/com/example/leaves/model/entity/EmployeeInfo.java
@@ -56,8 +56,8 @@ public class EmployeeInfo extends BaseEntity<EmployeeInfoDto> {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "employeeInfo", cascade = {CascadeType.ALL})
     private List<ContractEntity> contracts = new ArrayList<>();
 
-//    @OneToMany(mappedBy = "employeeInfo")
-//    private List<HistoryEntity> historyList = new ArrayList<>();
+    @OneToMany(mappedBy = "employeeInfo")
+    private List<HistoryEntity> historyList = new ArrayList<>();
 
     @ElementCollection
     @CollectionTable(name = "employee_info_history", joinColumns = @JoinColumn(name = "employee_info_id"))
